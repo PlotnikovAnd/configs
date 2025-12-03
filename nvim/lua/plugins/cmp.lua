a0511a961a6b7e05f3f2acbb4f9caebae48961a9
PlotnikovAnd <xdernx@yandex.ru>
2025-12-03 16:55:04 +0300

    redo cmp to enable snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,53 +1,82 @@
 return {
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-buffer' },
-	{ 'hrsh7th/cmp-path' },
-	{ 'hrsh7th/cmp-cmdline' },
-	{ 'neovim/nvim-lspconfig' },
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-vsnip' },
-	{ 'hrsh7th/vim-vsnip' },
 	{
-		'hrsh7th/nvim-cmp',
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
 		config = function()
-			local cmp = require('cmp')
-			vim.cmd [[ filetype plugin indent on ]]
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
-					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						luasnip.lsp_expand(args.body)
 					end,
 				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					['<Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
+					-- ПРОКРУТКА ДОКУМЕНТАЦИИ
+					-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+					-- ВЫЗОВ МЕНЮ ВРУЧНУЮ
+					["<C-Space>"] = cmp.mapping.complete(),
+
+					-- ЗАКРЫТЬ МЕНЮ
+					["<C-e>"] = cmp.mapping.abort(),
+
+					-- ПОДТВЕРДИТЬ ВЫБОР (Enter)
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+					-- == НАВИГАЦИЯ ПО МЕНЮ (Вместо Tab) ==
+					-- Используем стандартные Vim-хоткеи или удобные C-j/C-k
+					-- ["<C-n>"] = cmp.mapping.select_next_item(), -- Вниз
+					-- ["<C-p>"] = cmp.mapping.select_prev_item(), -- Вверх
+
+					-- Альтернатива (если вам удобнее j/k как в навигации по файлу)
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+
+					-- == ПРЫЖКИ ВНУТРИ СНИППЕТОВ ==
+					-- Tab будет работать как "прыжок" только ЕСЛИ вы внутри активного сниппета.
+					-- Во всех остальных случаях сработает ваш fallback (переключение табов).
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback() -- ВОТ ЗДЕСЬ сработает ваше переключение табов
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
 						else
 							fallback()
 						end
-					end, { 'i', 's' }),
-					['<S-Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						else
-							fallback()
-						end
-					end, { 'i', 's' })
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'vsnip' }, -- For vsnip users.
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
 				}, {
-					{ name = 'buffer' },
-				})
+					{ name = "buffer" },
+				}),
 			})
-		end
-	}
+		end,
+	},
 }
