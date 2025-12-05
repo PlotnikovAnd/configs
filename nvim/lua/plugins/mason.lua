d5d9abdcd10eb2933919c431e2ada8c4c01fa271
PlotnikovAnd <xdernx@yandex.ru>
2025-12-05 13:03:21 +0300

    fix formatters install by mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,17 +1,23 @@
 return {
+	-- 1. Сам Mason (Менеджер пакетов)
 	{
-		'mason-org/mason.nvim',
-		opts = {},
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗"
+		'williamboman/mason.nvim', -- Обратите внимание: оф. репозиторий williamboman, а не mason-org
+		cmd = "Mason",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
 			}
 		}
 	},
+
+	-- 2. Автоустановка LSP серверов (clangd, lua_ls и т.д.)
 	{
-		"mason-org/mason-lspconfig.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
 		config = function()
 			require('mason-lspconfig').setup {
 				ensure_installed = {
@@ -25,6 +31,27 @@ return {
 					"ruff",
 					"rust_analyzer",
 				}
+			}
+		end
+	},
+
+	-- 3. Автоустановка Форматтеров (Stylua, Prettier) и Линтеров
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require('mason-tool-installer').setup {
+				ensure_installed = {
+					-- Сюда пишем инструменты, которые НЕ являются LSP
+					"stylua", -- Форматтер для Lua
+					"prettier", -- Форматтер для JS/TS/HTML/CSS/JSON
+
+					-- Можно добавить еще:
+					-- "codelldb", -- Дебаггер для C++
+					-- "eslint_d", -- Линтер для JS
+				},
+				auto_update = true,
+				run_on_start = true,
 			}
 		end
 	},
