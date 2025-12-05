b30f6ff0445d99f4a8e837303002e1d758a1b7f9
PlotnikovAnd <xdernx@yandex.ru>
2025-12-05 13:04:36 +0300

    update conform linters invokation logic

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,25 +1,37 @@
 return {
 	{
-		'stevearc/conform.nvim',
-		opts = {},
-		config = function()
-			require("conform").setup(
-				{
-					formatters_by_ft = {
-						lua = { "stylua" },
-						python = { "isort", "black" },
-						rust = { "rustfmt", lsp_format = "fallback" },
-						javascript = { "prettier" },
-						xml = { "xmlformatter" },
-						-- cpp = { "clang-format", lsp_format = "fallback" }
-					}
-				})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			})
-		end
-	}
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" }, -- Загружать плагин перед сохранением файла
+		cmd = { "ConformInfo" }, -- И доступ к команде информации
+		opts = {
+			-- Настройка форматтеров под конкретные языки
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" }, -- Сначала отсортирует импорты, потом отформатирует код
+
+				-- Для Rust обычно используется LSP (rust-analyzer), но можно и rustfmt явно
+				rust = { "rustfmt" },
+
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				json = { "prettier" },
+
+				xml = { "xmlformatter" },
+
+				-- Для C++ и C оставляем пустым, чтобы сработал lsp_fallback (clangd)
+				cpp = {},
+				c = {},
+			},
+
+			-- Настройка форматирования при сохранении
+			format_on_save = {
+				timeout_ms = 500, -- Если форматирование идет дольше 0.5сек, не ждать
+
+				-- Самая важная настройка:
+				-- Если для языка не найден форматтер в списке выше (например для cpp),
+				-- то использовать LSP (clangd)
+				lsp_format = "fallback",
+			},
+		},
+	},
 }
