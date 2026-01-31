84ce3f78c5f3f82e6333933c4d982e2101e1d59d
PlotnikovAnd <xdernx@yandex.ru>
2026-01-31 18:08:44 +0300

    fix ll, ls, add lz, lS, lt

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,28 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# Init tools
+	starship init fish | source 
+	zoxide init --cmd cd fish | source
 	fzf --fish | source
-	# fzf preview settings
+
+	# Previews 
 	set -x FZF_CTRL_T_OPTS "--preview 'batcat -n --color=always --line-range :500 {}'"
 	set -x FZF_ALT_C_OPTS "--preview 'eza --tree --color=always {} | head -200'"
 
-	alias ls="eza --oneline --color=always --all --no-permissions --group --git --no-filesize --icons=always"
-	alias ll="eza --oneline --color=always --all --long --no-user --time-style=long-iso --total-size --group-directories-first"
+	# Aliases
 	alias cat="batcat"
 	alias nv="NVIM_APPNAME=nvchad nvim"
-	starship init fish | source 
-	eval (zellij setup --generate-auto-start fish | string collect)
-	zoxide init --cmd cd fish | source
+
+	# Eza config
+    alias ls="eza --grid --icons=auto --group-directories-first"
+    alias ll="eza --long --all --header --icons=auto --git --group-directories-first --time-style=long-iso --smart-group --octal-permissions"
+    alias lt="eza --tree --level=2 --icons=auto" 
+	alias lS="eza --long --header --sort=size --reverse --icons=auto"
+	# Как ll, но считает размер папок (--total-size) и подсвечивает вес (--color-scale)
+	alias lz="eza --long --all --header --icons=auto --git --group-directories-first --time-style=long-iso --smart-group --octal-permissions --total-size --color-scale=size"
+
+	# Path
 	fish_add_path ~/bin
+
+	# Zellij (лучше в конце, чтобы environment успел загрузиться)
+	eval (zellij setup --generate-auto-start fish | string collect)
 end
